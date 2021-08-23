4e28ab253d51e3f2f4c240fec712a4c1b725bf5a
Nick James <nicholas.james@sydney.edu.au>
2021-08-23 10:58:05 +1000

    Update for lamda 1(t) samples

--- myfile.py
+++ myfile.py
@@ -5,11 +5,9 @@ import random
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 
-np.random.seed(1234)
-
 # Choose number of sectors and n for simulation
-num_sectors = 3
-n = 30
+num_sectors = 10
+n = 50
 sample_per_sector = n//num_sectors
 
 # Import data
@@ -21,12 +19,11 @@ prices.columns = prices.columns.str.replace('(\.\d+)$','')
 
 sectors_labels = ["Health Care", "Industrials", "Information Technology", "Utilities", "Financials",
            "Materials", "Real Estate", "Consumer Staples", "Consumer Discretionary", "Energy", "Communication Services"]
-# sectors_labels = ["Consumer Discretionary", "Energy", "Communication Services", "Energy"]
 
 sectors_labels.sort()
 
 first_eigenvalue_samples = []
-returns_list = []
+portfolio_returns_sample = []
 while len(first_eigenvalue_samples) < 5:
     # First pick n sectors at random
     sector_sequence = list(np.linspace(0,len(sectors_labels)-1,len(sectors_labels))) # Randomly draw sector numbers
@@ -61,6 +58,7 @@ while len(first_eigenvalue_samples) < 5:
     log_returns = np.log(stock_samples_df).diff()[1:]
     smoothing_rate = 120
 
+    returns_list = []
     corr_1 = []
     for i in range(smoothing_rate, len(log_returns)):
         # Returns
@@ -75,22 +73,37 @@ while len(first_eigenvalue_samples) < 5:
         print("Iteration "+str(i)+" / "+str(len(log_returns)))
 
         # Compute total returns
-        returns_1 = np.array(log_returns.iloc[smoothing_rate, :])
+        returns_1 = np.array(log_returns.iloc[i, :])
         weights = np.repeat(1/len(returns_1), n)
-        total_return_iteration = np.sum(returns_1 * weights).flatten()
+        total_return_iteration = np.sum(returns_1 * weights)
         returns_list.append(total_return_iteration)
 
     # Append draws of first eigenvalue samples to main list
     first_eigenvalue_samples.append(corr_1)
+    portfolio_returns_sample.append(returns_list)
 
 # Plot first 5 samples
+avg_explanatory_variance = [] # Average explanatory variance from first eigenvalue
 for i in range(len(first_eigenvalue_samples)):
     plt.plot(first_eigenvalue_samples[i])
+    avg_explanatory_variance.append(np.mean(first_eigenvalue_samples[i]))
 plt.title("First eigenvalue samples")
 plt.show()
 
-# Plot 5 return paths
-for i in range(len(returns_list)):
-    plt.plot(returns_list[i])
-plt.title("Returns samples")
+# Plot distribution of first eigenvalue
+plt.hist(avg_explanatory_variance)
+plt.title("First eigenvalue average contribution")
 plt.show()
+
+# Plot 5 return paths
+for i in range(len(portfolio_returns_sample)):
+    plt.plot(portfolio_returns_sample[i], alpha=0.1)
+plt.title("Portfolio return samples")
+plt.show()
+
+# Compute Expected Return
+portfolio_returns_sample_array = np.array(portfolio_returns_sample)
+returns_expectation = portfolio_returns_sample_array.mean(axis=0)
+volatility_expectation = np.std(returns_expectation) * np.sqrt(250)
+print(returns_expectation)
+print(volatility_expectation)
