1adc062dd03c5484f3471890e259a4f07422cfdb
Nick James <nicholas.james@sydney.edu.au>
2021-09-20 13:27:48 +1000

    update for RMT

--- Clustering_lambda_1_paths.py
+++ Clustering_lambda_1_paths.py
@@ -5,8 +5,8 @@ import pandas as pd
 import glob
 
 # plot parameters
-plt.rc('xtick', labelsize=8)
-plt.rc('ytick', labelsize=8)
+plt.rc('xtick', labelsize=6)
+plt.rc('ytick', labelsize=6)
 
 labels = ["2,2","2,3","2,4","2,5","2,6","2,7","2,8","2,9",
           "3,2","3,3","3,4","3,5","3,6","3,7","3,8","3,9",
