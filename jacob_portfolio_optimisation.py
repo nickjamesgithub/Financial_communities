60012d8f3523eee0e1dae2ee13c45ea931ae8bd4
Nick James <nicholas.james@sydney.edu.au>
2022-08-22 09:59:07 +1000

    updates for jacob financial crises paper

--- jacob_portfolio_optimisation.py
+++ jacob_portfolio_optimisation.py
@@ -22,32 +22,49 @@ sectors = prices_clean.columns
 stocks = prices_clean.iloc[0,:].values
 
 if crisis == "dot_com":
-    prices = prices_clean.iloc[146:794,:]
+    prices = prices_clean.iloc[146:794,:].astype("float")
 
 if crisis == "gfc":
-    prices = prices_clean.iloc[1865:2703,:]
+    prices = prices_clean.iloc[1865:2703,:].astype("float")
 
 if crisis == "covid":
-    prices = prices_clean.iloc[5184:5304,:]
+    prices = prices_clean.iloc[5184:5304,:].astype("float")
 
 if crisis == "ukraine":
-    prices = prices_clean.iloc[5642:5733,:]
-
-# First pick n sectors at random
-sector_sequence = list(np.linspace(0, len(prices.iloc[0]) - 1, len(prices.iloc[0])))  # Randomly draw sector numbers
-random_list_sector = random.sample(sector_sequence, num_stocks)
-ints = [int(item) for item in random_list_sector]
-
-# Select random stocks
-random_price = prices.iloc[:, ints] # Slice specific stocks
-random_sectors = sectors[ints]
-random_stocks = stocks[ints]
-
-# Compute log returns of prices
-
+    prices = prices_clean.iloc[5642:5733,:].astype("float")
 
 # Store simulated values
 sim_sharpe = []
 sim_stocks = []
 sim_sectors = []
+
+while len(sim_sharpe) < 100:
+    # First pick n sectors at random
+    sector_sequence = list(np.linspace(0, len(prices.iloc[0]) - 1, len(prices.iloc[0])))  # Randomly draw sector numbers
+    random_list_sector = random.sample(sector_sequence, num_stocks)
+    ints = [int(item) for item in random_list_sector]
+
+    # Select random stocks
+    random_price = prices.iloc[:, ints] # Slice specific stocks
+    random_sectors = sectors[ints]
+    random_stocks = stocks[ints]
+
+    # Compute log returns of prices
+    log_returns = np.log(random_price).diff()[1:]
+    total_returns = np.sum(log_returns, axis=0)
+    cov = np.cov(np.transpose(log_returns))
+    weights = np.repeat(1/len(random_stocks), len(random_stocks))# .reshape(len(random_stocks),1)
+
+    # Compute portfolio returns
+    portfolio_returns = np.matmul(weights, total_returns)
+    portfolio_std = np.dot(np.transpose(weights), np.dot(cov, weights))
+    portfolio_sharpe = portfolio_returns/portfolio_std
+
+    # Append Sharpe, stocks and sector
+    sim_sharpe.append(portfolio_sharpe) # Append Sharpe Ratio
+    sim_sectors.append(random_sectors) # Append Sectors
+    sim_stocks.append(random_stocks) # Append Stocks
+
+    block = 1
+
 
